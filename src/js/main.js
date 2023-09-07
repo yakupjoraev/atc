6330afb7e052f4914216cd71e7de9e108c439360
Yakup <yakupjoraev@gmail.com>
2023-09-08 00:06:57 +0300

    added new page

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,117 @@
 // Custom scripts
-const swiper = new Swiper(".revievs__slider", {
-  slidesPerView: 1.5,
-  spaceBetween: 7,
-  pagination: {
-    el: ".revievs__slider-pagination",
-  },
-
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1.5,
-      spaceBetween: 7,
-    },
-    // when window width is >= 767px
-    767: {
-      slidesPerView: 2.5,
-      spaceBetween: 10,
-    },
-    // when window width is >= 992px
-    992: {
-      slidesPerView: 3.5,
-      spaceBetween: 14,
+// Мобильное меню бургер
+function burgerMenu() {
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.menu')
+  const body = document.querySelector('body')
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      burger.classList.add('active-burger')
+      body.classList.add('locked')
+    } else {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
     }
+  })
+  //снять классы при клике на элементы меню
+  const menuItems = document.querySelectorAll('.menu__item')
+
+  menuItems.forEach(item => {
+    item.addEventListener('click', function () {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    })
+  });
+
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+}
+burgerMenu()
+
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+  const nav = document.querySelector('nav')
+
+  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+  const breakpoint = 1
+  if (window.scrollY >= breakpoint) {
+    nav.classList.add('fixed__nav')
+  } else {
+    nav.classList.remove('fixed__nav')
   }
-});
+}
+window.addEventListener('scroll', fixedNav)
+
+
+function revievsSlider() {
+  const container = document.querySelector('.revievs');
+
+  if (!container) {
+    return null
+  }
+
+  const swiper = new Swiper(".revievs__slider", {
+    slidesPerView: 1.5,
+    spaceBetween: 7,
+    pagination: {
+      el: ".revievs__slider-pagination",
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1.5,
+        spaceBetween: 7,
+      },
+      // when window width is >= 767px
+      767: {
+        slidesPerView: 2.5,
+        spaceBetween: 10,
+      },
+      // when window width is >= 992px
+      992: {
+        slidesPerView: 3.5,
+        spaceBetween: 14,
+      }
+    }
+  });
+}
+revievsSlider();
+
+function heroSlider() {
+  const container = document.querySelector('.hero');
+
+  if (!container) {
+    return null
+  }
+
+  const swiper = new Swiper(".hero__slider", {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+
+    pagination: {
+      el: ".hero__slider-pagination",
+    },
+
+    navigation: {
+      nextEl: ".hero__slider--next",
+      prevEl: ".hero__slider--prev",
+    },
+  });
+}
+heroSlider();
